e9efad8ab2d6bc0749cecfc08f0ffe15bcc0fb87
Matthew Dippel <mdip226@gmail.com>
2021-03-29 17:53:12 -0400

    chart!

--- lib/providers/bitcoin_price.dart
+++ lib/providers/bitcoin_price.dart
@@ -4,16 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
+class PriceDatum {
+  PriceDatum(this.date, this.price);
+  final DateTime date;
+  final double price;
+}
+
 class BitcoinPrice with ChangeNotifier {
-  final _prices = [];
-  Future<List> fetchAndSetPrices() async {
+  List<PriceDatum> _prices = [];
+
+  List<PriceDatum> get getPrices {
+    return [..._prices];
+  }
+
+  void fetchAndSetPrices() async {
     try {
       //you can provide start and end parameters here
       final url =
           Uri.parse('https://api.coindesk.com/v1/bpi/historical/close.json');
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
-      print(extractedData);
+      final extractedBpi = extractedData["bpi"] as Map<String, dynamic>;
+      extractedBpi.forEach((key, value) {
+        _prices.add(PriceDatum(DateTime.parse(key), value));
+      });
+      print(_prices);
     } catch (e) {
       print(e);
     }
--- lib/screens/price_screen.dart
+++ lib/screens/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:perlemir_mobile/providers/bitcoin_price.dart';
 import 'package:perlemir_mobile/widgets/app_drawer.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class PriceScreen extends StatefulWidget {
   static const routeName = '/price-screen';
@@ -24,11 +25,23 @@ class _PriceScreenState extends State<PriceScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final _data = Provider.of<BitcoinPrice>(context).getPrices;
     return Scaffold(
       appBar: AppBar(
         title: Text("Live Pricing"),
       ),
       drawer: AppDrawer(),
+      body: Center(
+        child: SfCartesianChart(
+            primaryXAxis: DateTimeAxis(),
+            primaryYAxis: NumericAxis(),
+            series: <ChartSeries>[
+              LineSeries<PriceDatum, DateTime>(
+                  dataSource: _data,
+                  xValueMapper: (datum, _) => datum.date,
+                  yValueMapper: (datum, _) => datum.price),
+            ]),
+      ),
     );
   }
 }
